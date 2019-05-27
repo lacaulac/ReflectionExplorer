ae1ecdd2f045197183a65bba0c3e5a4ea44357b4
lacaulac <antonin17@live.fr>
2019-05-27 17:44:22 +0200

    First commit

---
+++ ReflectionExplorer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ReflectionExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.Enabled = false;
+            this.filePath.Location = new System.Drawing.Point(101, 10);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(617, 20);
+            this.filePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".NET Assembly:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(724, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BrowseButton_click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(16, 36);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(702, 402);
+            this.treeView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ ReflectionExplorer/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReflectionExplorer
+{
+    public partial class Form1 : Form
+    {
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = ".NET Assembly files (*.dll)|*.dll|.NET Executable files (*.exe)|*.exe|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                InfoHolder.asm = Assembly.LoadFile(path);
+                filePath.Text = path;
+                updateTreeView();
+            }
+        }
+
+        public void updateTreeView()
+        {
+            foreach(Type t in InfoHolder.asm.GetTypes())
+            {
+                TreeNode tmp = treeView1.Nodes.Add(t.FullName);
+                tmp.Tag = t;
+                foreach(MethodInfo mt in t.GetMethods())
+                {
+                    ParameterInfo[] pms = mt.GetParameters();
+                    StringBuilder str = new StringBuilder();
+                    for(int i=0; i<pms.Length; i++)
+                    {
+                        str.Append(string.Format("{1} {0}", pms[i].Name, pms[i].ParameterType.Name));
+                        if(i != pms.Length - 1)
+                        {
+                            str.Append(", ");
+                        }
+                    }
+                    tmp.Nodes.Add(string.Format("{4} {3} {1} {0}({2})", mt.Name, mt.ReturnType.Name, str.ToString(), mt.IsStatic?"static":"", (mt.IsPrivate?"private":(mt.IsPublic?"public":"protected"))));
+                }
+            }
+        }
+    }
+}
